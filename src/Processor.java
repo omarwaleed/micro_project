d546bfaa1a57cfb27a8b0305a0c61b57a236060a
omarwaleed <omarpersonalprofile@gmail.com>
2015-11-16 12:03:05 +0200

    added architecture to the processor and cache

--- src/Processor.java
+++ src/Processor.java
@@ -1,15 +1,21 @@
+import java.util.ArrayList;
+
 // this class handles the processor functions and contains all the processor registers
 
 public class Processor {
 	
+//	Initialize the registers and the cache levels
+//	cache levels will end with the memory level but to be handled after user input
 	private static int[] register = new int[32];
+	private ArrayList <Cache> cacheLevel = new ArrayList<Cache>();
 
-//	get the value inside a register
+//	get the value inside a single register
 	public int getRegister(int reg) 
 	{
 		return register[reg];
 	}
 
+//	set the value of a single register
 	public void setRegister(int reg, int value) 
 	{
 //		checks on an attempt to modify register 0 if so ignore the setting
