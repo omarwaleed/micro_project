8103f5694d94fd5f168fcd8f1a5f49a862cc1098
amallasheen <amalwalied@gmail.com>
2015-11-21 13:51:24 +0200

    #4 started Direct map writes, tested it, awaiting cache read testing

--- src/Cache.java
+++ src/Cache.java
@@ -1,11 +1,17 @@
+import java.util.Arrays;
+
 
 public class Cache 
 {
-	private int[][] content;
+	private String[][] content;
 	private int assoc;
 	private int writePolicy; // write policy will be indicated by an int value to be discussed later
 	private int cycles;
-	
+	private int size;
+	private int lineSize;
+	private int hitRate = 0;
+	 //valid bit is stored at index content[n][content[n].length-1]
+	//tag bit is stored at index content[n][content[n].length-2]
 	
 //	initialize the cache with given parameters
 	public Cache(int size, int lineSize, int associativity) 
@@ -18,7 +24,9 @@ public class Cache
 		}
 		
 //		size of each indexed value will depend on the line size since the cache has a fixed number of inputs
-		content = new int[size/lineSize][lineSize];
+		this.size = size;
+		this.lineSize = lineSize;
+		content = new String[size/(lineSize/2)][lineSize];
 		assoc = associativity;
 	}
 	
@@ -33,8 +41,12 @@ public class Cache
 		}
 		writePolicy = writePolicyParam;
 		cycles = cyclesParam;
-		content = new int[size/lineSize][lineSize];
 		assoc = associativity;
+		this.size = size;
+		this.lineSize = lineSize;
+		if (assoc ==1) {		
+		 content = new String[size/(lineSize/2)][lineSize+2];
+		}
 	}
 	
 	
@@ -59,9 +71,115 @@ public class Cache
 		this.cycles = cycles;
 	}
 	
-	public int[] getContentOf(int index) 
+	public String[] getContentOf(int index) 
 	{
 		return this.content[index];
 	}
-
+	// method to check if the data is already there
+	public boolean hitOrMissDM(int address) {
+		//int[] division = divide(address);
+        int blockNo = size/lineSize;
+       // System.err.println("Division: "+Arrays.toString(division));
+        int index =  divide(address)[1];
+       // System.out.println("index in hitOrMiss " + index);
+        int tag =  divide(address)[0];
+        System.out.println("index " + index + " tag " + tag);
+       // System.err.println("index value " + index + " "+content.length );
+         if (content != null && content[index] != null && content[index][content[index].length-1] != null && content[index][content[index].length-2] != null){
+        if (content[index][content[index].length-1].equals("0") || !content[index][content[index].length-2].equals(tag+""))
+		  return false;
+        else 
+        	return true;
+         }
+         return false;
+            
+	}
+	//method to read from cache 
+   public String[] readDM(int address) {
+	   int blockNo = size/lineSize;
+       int index = divide(address)[0];
+      
+	   if (hitOrMissDM(address)) {
+              hitRate++;
+		   return getContentOf(index);
+	   }
+	  return null;
+   }
+   //method to write to the cache
+   public void writeDM(int address,String[] data) { // bug in the tag and index use the binary thing
+	   //contentString();
+	  
+	   int blockNo = size/lineSize;
+	   int index = divide(address)[1];
+	   int offset = getOffset();
+	 //  System.err.println("Hit? : " + hitOrMissDM(address) + " block number " + blockNo);
+	   if (hitOrMissDM(address)) {
+		   content[index] = data; 
+		   hitRate++;
+		 
+		   if (writePolicy == 1) {
+			   // set the dirty bit here
+		   }
+	   }
+	   else {
+		   if (writePolicy == 1) {
+			   // write back method here 
+			   
+		   }
+		   int tag = divide(address)[0];
+		   String[] newData = new String[data.length+2];
+		   for(int i = 0; i<data.length;i++) {
+			   newData[i] = data[i];
+		   }
+		   newData[data.length] = tag+"";
+		   newData[data.length+1] = "1";
+		 //  System.err.println(Arrays.toString(newData));
+		   content[index] = newData; // replace 
+	   }
+	   
+   }
+   public int getOffset() {
+	   
+	   return 0;
+   }
+   public int getHitRate() {
+	   return hitRate;
+   }
+   public void clearCache() {
+	   content = new String[size/lineSize][lineSize+2];
+   }
+   public void contentString() {
+	   for(int i = 0; i<content.length;i++) {
+		   for(int j = 0; j < content[i].length;j++) {
+			   if (j == 0)
+				   System.out.print("["+content[i][j]);
+			   else if (j == content[0].length-1) 
+				   System.out.print(", "+content[i][j]+"]");
+			   else 
+				   System.out.print(", " + content[i][j]);		   
+		   }
+		   System.out.println();
+	   }
+	   
+   }
+   public int[] divide(int address) {
+	   String binary = Integer.toBinaryString(address);
+	  /// System.out.println(binary.length());
+	   for (int i = binary.length(); i< 32 ;i++) {
+		   binary = "0" + binary;
+	   }
+	  // System.out.println("BinaryString: " + binary);
+	   int indexBits = (int)(Math.log((size/(lineSize)))/Math.log(2))+1;
+	   int offsetBits = (int)(Math.log(lineSize)/Math.log(2));
+	   int tagBits = 32-(indexBits+offsetBits);
+	//   System.out.println("Index bits: " + indexBits + " Offset bits: " + offsetBits + " Tag bits: " + tagBits);
+	   int offset = Integer.parseInt(binary.substring(binary.length()-offsetBits, binary.length()),2);
+	 //  System.out.println("Offset: " + offset);
+	   int tag = Integer.parseInt(binary.substring(0,binary.length()-offsetBits-indexBits),2);
+	 //  System.out.println("Tag: " + tag);
+	   int index = Integer.parseInt(binary.substring(binary.length()-(offsetBits+indexBits),binary.length()-offsetBits),2);
+	  // System.out.println("index value: " + index);
+	   int[] division = {tag,index,offset};
+	   return division;
+   }
 }
---
+++ src/CacheTest.java
@@ -0,0 +1,83 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class CacheTest {
+ 
+    @Test
+    public void testHitorMissDirectMapped() {
+    	Cache c = new Cache(32, 4, 1, 0, 1);
+        String[] s = {"Hello","World"};
+    	c.writeDM(1*4, s);
+    	c.writeDM(134*4,s);
+    	c.writeDM(212*4, s);
+    	c.writeDM(1*4, s);
+    	c.writeDM(135*4,s);
+    	c.writeDM(213*4,s);
+    	c.writeDM(162*4,s);
+    	c.writeDM(161*4,s);
+    	c.writeDM(2*4,s);
+    	c.writeDM(44*4,s);
+    	c.writeDM(41*4,s);
+    	c.writeDM(221*4,s);
+    	
+    	assertEquals(1, c.getHitRate());
+    }
+    @Test
+    public void testHitorMissDirectMapped2() {
+    	Cache c = new Cache(32, 8, 1, 0, 1);
+        String[] s = {"Hello","World","Hello"};
+    	c.writeDM(1*4, s);
+    	c.writeDM(134*4,s);
+    	c.writeDM(212*4, s);
+    	c.writeDM(1*4, s);
+    	c.writeDM(135*4,s);
+    	c.writeDM(213*4,s);
+    	c.writeDM(162*4,s);
+    	c.writeDM(161*4,s);
+    	c.writeDM(2*4,s);
+    	c.writeDM(44*4,s);
+    	c.writeDM(41*4,s);
+    	c.writeDM(221*4,s);
+    	
+    	assertEquals(3, c.getHitRate());
+    }
+    @Test
+    public void testHitorMissDirectMappe3() {
+    	Cache c = new Cache(32, 16, 1, 0, 1);
+        String[] s = {"Hello","World","Hello"};
+    	c.writeDM(1*4, s);
+    	c.writeDM(134*4,s);
+    	c.writeDM(212*4, s);
+    	c.writeDM(1*4, s);
+    	c.writeDM(135*4,s);
+    	c.writeDM(213*4,s);
+    	c.writeDM(162*4,s);
+    	c.writeDM(161*4,s);
+    	c.writeDM(2*4,s);
+    	c.writeDM(44*4,s);
+    	c.writeDM(41*4,s);
+    	c.writeDM(221*4,s);
+    	
+    	assertEquals(2, c.getHitRate());
+    }
+    @Test 
+    public void testDivide() {
+    	Cache c = new Cache(32, 4, 1, 0, 1);
+    	int [] expected = {0,1,0};
+        assertArrayEquals(expected,c.divide(4));
+    	
+    }
+    @Test
+    public void testDivide2() {
+    	Cache c = new Cache(32, 4, 1, 0, 1);
+    	 int [] expected1 = {2,9,0};
+         assertArrayEquals(expected1,c.divide(41*4));
+    }
+    
+    @Test
+    public void testDivide3() {
+    	Cache c = new Cache(32, 4, 1, 0, 1);
+    	 int [] expected1 = {13,13,0};
+         assertArrayEquals(expected1,c.divide(221*4));
+    }
+}
